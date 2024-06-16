24242e22b8031d547fff7b1957a201e039046306
Jashkumar Solanki <jashkumarsolanki@gmail.com>
2024-06-16 11:00:31 +0530

    WorkManager Status Logic

--- app/src/main/java/com/example/workmanagerapp/MainActivity.java
+++ app/src/main/java/com/example/workmanagerapp/MainActivity.java
@@ -4,13 +4,16 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
 import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
 
@@ -35,6 +38,19 @@ public class MainActivity extends AppCompatActivity {
                 WorkManager.getInstance(getApplicationContext()).enqueue(wr);
             }
         });
+
+        WorkManager.getInstance(getApplicationContext())
+                        .getWorkInfoByIdLiveData(wr.getId())
+                                .observe(this, new Observer<WorkInfo>() {
+                                    @Override
+                                    public void onChanged(WorkInfo workInfo) {
+                                        if (workInfo != null){
+                                            Toast.makeText(MainActivity.this
+                                                    ,"Status"+workInfo.getState().name()
+                                                    ,Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
 
 
 
